e49f1ea50f301832f4b7cb4f549d070bbd112685
Maxence ABRILE <maxenceabrile@icloud.com>
2025-10-21 20:03:00 +0200

    feat: ADD A REALLY GREAT AND WORKING OPTIMISATION !

--- lib/dpll.ml
+++ lib/dpll.ml
@@ -1,5 +1,7 @@
 open List
 
+open Sat_types
+
 type literal = int
 type clause = literal list
 type cnf = clause list
@@ -15,79 +17,106 @@ let cnf_of_int_list_list (l: int list list) : cnf = l
 
 (** print_modele : result -> unit afficher le résultat *)
 let print_result: result -> unit = function
-  | Unsat   -> print_string "UNSAT\n"
-  | Sat modele -> print_string "SAT\n";
-     let modele_trie = sort (fun i j -> (abs i) - (abs j)) modele in
-     List.iter (fun i -> print_int i; print_string " ") modele_trie;
-     print_string "0\n"
+	| Unsat   -> print_string "UNSAT\n"
+	| Sat modele -> print_string "SAT\n";
+		let modele_trie = sort (fun i j -> (abs i) - (abs j)) modele in
+		List.iter (fun i -> print_int i; print_string " ") modele_trie;
+		print_string "0\n"
+;;
 
 (** simplifie : literal -> cnf -> cnf 
    applique la simplification de l'ensemble des clauses en mettant
    le littéral l à vrai *)
 
 let rec remove_literal_from_clause l clause = match clause with
-  | [] -> []
-  | hd::tl -> if hd = l then remove_literal_from_clause l tl else hd::remove_literal_from_clause l tl;;
+	| [] -> []
+	| hd::tl -> if hd = l then remove_literal_from_clause l tl else hd::remove_literal_from_clause l tl;;
 
-let simplifie l clauses = 
-  List.filter (fun clause -> not (List.mem l clause)) clauses
-  |> List.map (fun clause -> remove_literal_from_clause (-l) clause)
-(** Solveur dpll récursif *)
-
-(** pur : cnf -> literal option
-    - si `clauses' contient au moins un littéral pur, retourne
-      ce littéral ;
-    - sinon renvoie None *)
-let rec is_pure l clauses = match clauses with
-  | [] -> false
-  | hd::tl -> if List.mem (-l) hd then false else is_pure l tl
+let simplifie (state: Solver_state.t) (l: literal) (clauses: cnf) =
+	Solver_state.remove_literal state l;
+	Solver_state.remove_literal state (-l);
+	List.filter (fun clause -> not (List.mem l clause)) clauses
+	|> List.map (fun clause -> remove_literal_from_clause (-l) clause)
 ;;
 
-let rec pur clauses = match clauses with
-  | [] -> None
-  | hd::tl -> let rec aux = function
-      | [] -> pur tl
-      | hd'::tl' -> if is_pure hd' clauses then Some(hd') else aux tl'
-    in aux hd;;
-;;
+(* ----------------- Solveur dpll récursif ----------------- *)
 
 (** unitaire : cnf -> literal option
-    - si `clauses' contient au moins une clause unitaire, retourne
-      le littéral de cette clause unitaire ;
-    - sinon renvoie None *)
-let is_singleton elt = List.length elt = 1;;
+	- si `clauses' contient au moins une clause unitaire, retourne
+	  le littéral de cette clause unitaire ;
+	- sinon renvoie None *)
 let rec unitaire = function
-  | [] -> None
-  | [x]::tl -> Some x
-  | _::tl -> unitaire tl
+	| [] -> None
+	| [x]::_ -> Some x
+	| _::tl -> unitaire tl
 ;;
 
-let is_clause_empty clauses = List.exists (fun c -> c = []) clauses
+(** pur : cnf -> literal option
+	- si `clauses' contient au moins un littéral pur, retourne
+	  ce littéral ;
+	- sinon renvoie None *)
+let pur (state: Solver_state.t) = 
+	match Solver_state.NodeSet.max_elt_opt (state.literals_polarisation) with
+	| None -> None
+	| Some node ->
+		if Solver_state.literal_exists state (-node.literal) 
+		then None
+		else Some (node.literal)
+;;
 
-let rec get_random_literal clauses = match clauses with
-  | [] -> None
-  | hd::tl ->
-    match hd with
-    | [] -> get_random_literal tl
-    | x::_ -> Some x
+let is_clause_empty clauses = List.exists (fun c -> c = []) clauses;;
 
-let rec solveur_dpll_rec clauses interpretation =
-  if clauses = [] then Sat interpretation
-  else if is_clause_empty clauses then Unsat
-  else
-    match unitaire clauses with
-    | Some l -> solveur_dpll_rec (simplifie l clauses) (l::interpretation)
-    | None ->
-      match pur clauses with
-      | Some l -> solveur_dpll_rec (simplifie l clauses) (l::interpretation)
-      | None ->
-        match get_random_literal clauses with
-        | None -> Unsat (* plus là par sécurité, mais on a déjà traité [] plus haut *)
-        | Some l ->
-          match solveur_dpll_rec (simplifie l clauses) (l::interpretation) with
-          | Sat _ as s -> s
-          | Unsat -> solveur_dpll_rec (simplifie (-l) clauses) ((-l)::interpretation)
+let get_random_literal (state: Solver_state.t) = 
+	match Solver_state.NodeSet.max_elt_opt (state.literals_polarisation) with
+	| None -> None
+	| Some node -> Some (node.literal)
+;;
+
+let rec solveur_dpll_rec (state: Solver_state.t) (clauses: cnf) (inter: interpretation) =
+	(* Solver_state.dump_memory state; *)
+	if clauses = [] then Sat inter
+	else 
+	if is_clause_empty clauses
+	then Unsat
+	else
+		match unitaire clauses with
+		| Some l ->
+			let save = Solver_state.make_save state l in
+			let res = solveur_dpll_rec state (simplifie state l clauses) (l::inter) in (
+				match res with
+				| Sat _ -> res
+				| Unsat -> Solver_state.restore_save state save; Unsat
+			)
+		| None -> 
+			match pur state with
+			| Some l -> 
+				let save = Solver_state.make_save state l in
+				let res = solveur_dpll_rec state (simplifie state l clauses) (l::inter) in (
+					match res with
+					| Sat _ -> res
+					| Unsat -> Solver_state.restore_save state save; Unsat
+				)
+			| None ->
+				match get_random_literal state with
+				| None -> Unsat (* plus là par sécurité, mais on a déjà traité [] plus haut *)
+				| Some l ->
+					let save = Solver_state.make_save state l in
+					let res = solveur_dpll_rec state (simplifie state l clauses) (l::inter) in (
+						match res with
+						| Sat _ -> res
+						| Unsat -> 
+							(* (Logger.log Logger.SUCCESS "Touching bottom"); *)
+							Solver_state.restore_save state save;
+							let save = Solver_state.make_save state l in
+							let res = solveur_dpll_rec state (simplifie state (-l) clauses) ((-l)::inter) in (
+								match res with
+								| Sat _ -> res
+								| Unsat -> Solver_state.restore_save state save; Unsat
+							)
+					)
 ;;
 
 (* let dpll_solver clauses = solveur_dpll_rec clauses [];; *)
-let dpll_solver clauses = solveur_dpll_rec (Sat_types.Solver_state.create clauses).clauses [];;
+let dpll_solver clauses = 
+	let state = Solver_state.create clauses in
+	 solveur_dpll_rec state clauses [];;
--- lib/sat_types.ml
+++ lib/sat_types.ml
@@ -16,10 +16,13 @@ module Node = struct
 	let create (l: int) (ps: float) = {literal = l; polarity_score = ps};;
 
 	let compare (a: t) (b: t) = 
-		let res = Float.compare a.polarity_score b.polarity_score in
-		if res <> 0
-		then res
-		else Int.compare a.literal b.literal
+		if a.literal = b.literal (* Avoid having two times the same literal *)
+		then 0
+		else 
+			let res = Float.compare a.polarity_score b.polarity_score in
+			if res <> 0
+			then res
+			else Int.compare a.literal b.literal
 	;;
 	
 end;;
@@ -29,12 +32,15 @@ module Solver_state = struct
 
 	type lit_table_type = (int, int) Hashtbl.t
 
+	type save_type = (literal * int * Node.t) list
+
 	type t = {
 		lit_table: lit_table_type;
-		pure_literals: NodeSet.t;
+		mutable literals_polarisation: NodeSet.t;
 		clauses: cnf;
 	}
 
+	(* ----------------- Utils functions ----------------- *)
 	let append_literals_to_hashtbl (hash_tbl: lit_table_type) (clauses: cnf): unit =
 		List.iter (
 			fun clause -> List.iter (
@@ -68,18 +74,104 @@ module Solver_state = struct
 		hash_tbl
 		NodeSet.empty
 	;;
-	
+
+	let add_literal_to_node_set (set: NodeSet.t) (hash_tbl: lit_table_type) (l: literal): NodeSet.t =
+		(* Logger.log Logger.SUCCESS "Adding literal from node set"; *)
+		NodeSet.add (Node.create l (calculate_polarity_score hash_tbl l)) set
+	;;
+
+	let remove_literal_of_node_set (set: NodeSet.t) (l: literal): NodeSet.t =
+		(* Logger.log Logger.WARNING "Removing literal from node set"; *)
+		NodeSet.filter (fun node -> node.Node.literal <> l) set
+	;;
+
+	let find_node_by_literal (set: NodeSet.t) (l: literal) : Node.t =
+		(* Logger.log Logger.WARNING (Printf.sprintf "Finding node where l = %d in %d elements" l (NodeSet.cardinal set)); *)
+		(* Using filter+choose because without knowing why, NodeSet.find_first doesn't work... *)
+		NodeSet.choose (
+			NodeSet.filter (
+				fun elt -> (
+					(* Printf.printf "%d %f\n" elt.literal elt.polarity_score *)
+				); 
+				elt.literal = l
+			) 
+			set
+		)
+	;;
+		
+	(* ----------------- API Funcions ----------------- *)
+	let dump_memory (state: t): unit =
+		Logger.log Logger.WARNING "----------------------------------------------------";
+		Logger.log Logger.WARNING "------------------ DUMPING MEMORY ------------------";
+		Logger.log Logger.WARNING "----------------------------------------------------";
+		Printf.sprintf "Literal table size: %d" (Hashtbl.length state.lit_table) |> Logger.log Logger.INFO;
+		Printf.sprintf "Literal binary heap size: %d" (NodeSet.cardinal state.literals_polarisation) |> Logger.log Logger.INFO;
+		Hashtbl.iter (fun key value ->
+			Printf.sprintf "| %d | %d |" key value |> Logger.log Logger.INFO
+		) state.lit_table;
+
+		Printf.printf "\n";
+
+		NodeSet.iter (fun node ->
+			Printf.sprintf "{literal: %d; score: %s }" node.literal (if node.polarity_score = Float.max_float then "+inf" else string_of_float node.polarity_score) |> Logger.log Logger.INFO
+		) state.literals_polarisation
+	;;
+
 	(* TODO: Maybe instead of 64, calculate an approximate table size with [max (length clause) * (length clauses)] * 2 *)
 	let create (clauses: cnf): t =
 		(* Creates the Hash table *)
-		let lit_table = Hashtbl.create 64 in append_literals_to_hashtbl lit_table clauses;
+		let lit_table = Hashtbl.create 4096 in append_literals_to_hashtbl lit_table clauses;
 		(* Creates the sets *)
-		let pure_literals = keys_to_set_with_polarisation lit_table in
+		let literals_polarisation = keys_to_set_with_polarisation lit_table in
 		{
 			lit_table = lit_table;
-			pure_literals = pure_literals;
+			literals_polarisation = literals_polarisation;
 			clauses = clauses;
 		}
 	;;
-	
+
+	let literal_exists (state: t) (l: literal): bool = 
+		match Hashtbl.find_opt state.lit_table l with
+		| None -> false
+		| Some _ -> true
+	;;
+
+	let remove_literal (state: t) (l: literal) =
+		(* Logger.log Logger.WARNING (Printf.sprintf "Simplifying by %d" l);  *)
+		Hashtbl.remove state.lit_table l;
+		state.literals_polarisation <- (remove_literal_of_node_set (state.literals_polarisation) (l))
+	;;
+
+	(* ----------------- Saving functions ----------------- *)
+	let make_save (state: t) (l: literal): save_type =
+		(* dump_memory state; *)
+		let entry_for k =
+			(* Logger.log Logger.WARNING (Printf.sprintf "Making save on %d" k); *)
+			let old_count = Hashtbl.find state.lit_table k in (* find and not find_opt because no bugs are supposed to happend except if the Hashtbl is empty*)
+			let old_node = find_node_by_literal state.literals_polarisation k in
+			(k, old_count, old_node)
+		in
+		if (literal_exists state (-l)) then [entry_for l; entry_for (-l)] else [entry_for l]
+	;;
+
+	let restore_save (state: t) (save: save_type): unit =
+		(* Logger.log Logger.ERROR "Restoring save"; *)
+		(* dump_memory state; *)
+		List.iter (fun (key, old_count, _) ->
+			(* Logger.log Logger.ERROR (string_of_int key);  *)
+			Hashtbl.replace state.lit_table key old_count
+		)
+		save;
+
+		List.iter (fun (key, _, old_node) ->
+			state.literals_polarisation <- (add_literal_to_node_set state.literals_polarisation state.lit_table key)
+		)
+		save;
+
+		(* Logger.log Logger.ERROR "Memory restored"; *)
+		(* dump_memory state; *)
+	;;
 end
+
+
+(* BUG: On restore l et -l puis on simplifie (au retour du backtrack) sans avoir refait de save avant... *)
